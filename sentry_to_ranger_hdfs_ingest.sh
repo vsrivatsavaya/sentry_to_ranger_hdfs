3cf6385e0b5c84541b628e101c5471f134aa5705
vsrivatsavaya <126549336+vsrivatsavaya@users.noreply.github.com>
2023-10-20 13:43:44 -0400

    Update sentry_to_ranger_hdfs_ingest.sh

--- sentry_to_ranger_hdfs_ingest.sh
+++ sentry_to_ranger_hdfs_ingest.sh
@@ -5,29 +5,25 @@ ranger_url="https://vvs-cdpchf15-ec2-4.vpc.cloudera.com:6182"
 ranger_admin_username="admin"
 ranger_admin_password="Cloudera123"
 
-rm -f ranger_hdfs_policy.json
-rm -f service_mapping_json
-
+echo -e '\n'
 echo "$(date) - Sentry to Ranger HDFS policy ingest started"
+echo -e '\n'
 
-policy_json='{"policies": ['
 while IFS="," read -r policy_name resource permissions role
 do
   if [ $permissions == 'rwx' ]; then
-  policy_json+='{"policyType":"0","name":"'$policy_name'","isEnabled":true,"policyPriority":0,"policyLabels":[],"description":"","isAuditEnabled":true,"resources":{"path":{"values":["'$resource'"],"isRecursive":true}},"isDenyAllElse":false,"policyItems":[{"roles":["'$role'"],"accesses":[{"type":"read","isAllowed":true},{"type":"write","isAllowed":true},{"type":"execute","isAllowed":true}]}],"allowExceptions":[],"denyPolicyItems":[],"denyExceptions":[],"service":"cm_hdfs"},'
+  policy_json='{"policyType":"0","name":"'$policy_name'","isEnabled":true,"policyPriority":0,"policyLabels":[],"description":"","isAuditEnabled":true,"resources":{"path":{"values":["'$resource'"],"isRecursive":true}},"isDenyAllElse":false,"policyItems":[{"roles":["'$role'"],"accesses":[{"type":"read","isAllowed":true},{"type":"write","isAllowed":true},{"type":"execute","isAllowed":true}]}],"allowExceptions":[],"denyPolicyItems":[],"denyExceptions":[],"service":"cm_hdfs"}'
   fi
   if [ $permissions == 'rx' ]; then
-  policy_json+='{"policyType":"0","name":"'$policy_name'","isEnabled":true,"policyPriority":0,"policyLabels":[],"description":"","isAuditEnabled":true,"resources":{"path":{"values":["'$resource'"],"isRecursive":true}},"isDenyAllElse":false,"policyItems":[{"roles":["'$role'"],"accesses":[{"type":"read","isAllowed":true},{"type":"execute","isAllowed":true}]}],"allowExceptions":[],"denyPolicyItems":[],"denyExceptions":[],"service":"cm_hdfs"},'
+  policy_json='{"policyType":"0","name":"'$policy_name'","isEnabled":true,"policyPriority":0,"policyLabels":[],"description":"","isAuditEnabled":true,"resources":{"path":{"values":["'$resource'"],"isRecursive":true}},"isDenyAllElse":false,"policyItems":[{"roles":["'$role'"],"accesses":[{"type":"read","isAllowed":true},{"type":"execute","isAllowed":true}]}],"allowExceptions":[],"denyPolicyItems":[],"denyExceptions":[],"service":"cm_hdfs"}'
   fi
+
+  curl --noproxy '*' -X POST -H "Content-Type: application/json" -d "$policy_json" -k -u $ranger_admin_username:$ranger_admin_password "$ranger_url/service/plugins/policies?isOverride=false&mergeIfExists=true"
+  echo -e '\n'
 done < <(tail -n +2 $input_feed_file)
 
-policy_json=${policy_json%?}
-policy_json+=']}'
-echo "$policy_json" > ranger_hdfs_policy.json
-echo '{"cm_hdfs":"cm_hdfs"}' > service_mapping_json
-
-curl --noproxy '*' -X POST -H "Content-Type: multipart/form-data" -F 'file=@ranger_hdfs_policy.json' -F "servicesMapJson=@service_mapping_json" -k -u $ranger_admin_username:$ranger_admin_password "$ranger_url/service/plugins/policies/importPoliciesFromFile?isOverride=false&mergeIfExists=true"
-
+echo -e '\n'
 echo "$(date) - Sentry to Ranger HDFS policy ingest completed"
+echo -e '\n'
 
 exit 0
